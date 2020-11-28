1dba196d9892dd36668e978bfcba621cf203ef6c
Mil Amirian <milamirian22@gmail.com>
2020-11-28 20:45:03 +0000

    revised the handleChange method on base.jsx

--- client/components/base.jsx
+++ client/components/base.jsx
@@ -27,9 +27,6 @@ export default class Base extends React.Component {
         crust: e.target.value
       });
     }
-
-    this.props.pizzaBase(this.state.size, this.state.crust);
-
   }
 
   render() {
@@ -64,25 +61,25 @@ export default class Base extends React.Component {
                       <h2 className="form-headers pl-4">Size</h2>
                       <div className="choices ml-4">
                         <div className="form-check topping-items">
-                          <input className="form-check-input" type="radio" name="size" id="small" value="small" onChange={this.handleChange} />
+                          <input className="form-check-input" type="radio" name="size" id="small" value="small" onChange={() => { this.props.pizzaBase(event.target.value); }} />
                           <label className="form-check-label" htmlFor="small">
                             <span>Small</span> (6 slices)
                           </label>
                         </div>
                         <div className="form-check topping-items">
-                          <input className="form-check-input" type="radio" name="size" id="medium" value="medium" onChange={ this.handleChange } />
+                          <input className="form-check-input" type="radio" name="size" id="medium" value="medium" onChange={() => { this.props.pizzaBase(event.target.value); }} />
                           <label className="form-check-label" htmlFor="medium">
                             <span>Medium</span> (8 slices)
                           </label>
                         </div>
                         <div className="form-check topping-items">
-                          <input className="form-check-input" type="radio" name="size" id="large" value="large" onChange={ this.handleChange }/>
+                          <input className="form-check-input" type="radio" name="size" id="large" value="large" onChange={() => { this.props.pizzaBase(event.target.value); }}/>
                           <label className="form-check-label" htmlFor="large">
                             <span>Large</span> (10 slices)
                           </label>
                         </div>
                         <div className="form-check topping-items">
-                          <input className="form-check-input" type="radio" name="size" id="extra-large" value="extra-large" onChange={ this.handleChange } />
+                          <input className="form-check-input" type="radio" name="size" id="extra-large" value="extra-large" onChange={() => { this.props.pizzaBase(event.target.value); }} />
                           <label className="form-check-label" htmlFor="extra-large">
                             <span>Extra-Large</span> (14 slices)
                           </label>
@@ -93,19 +90,19 @@ export default class Base extends React.Component {
                       <h2 className="form-headers pl-4">Crust</h2>
                       <div className="choices ml-4">
                         <div className="form-check topping-items">
-                          <input className="form-check-input" type="radio" name="crust" id="exampleRadios2" value="regular" onChange={ this.handleChange } />
+                          <input className="form-check-input" type="radio" name="crust" id="exampleRadios2" value="regular" onChange={() => { this.props.pizzaCrust(event.target.value); }} />
                           <label className="form-check-label" htmlFor="regular">
                             <span>Regular</span>
                           </label>
                         </div>
                         <div className="form-check topping-items">
-                          <input className="form-check-input" type="radio" name="crust" id="exampleRadios3" value="thin" onChange={ this.handleChange } />
+                          <input className="form-check-input" type="radio" name="crust" id="exampleRadios3" value="thin" onChange={() => { this.props.pizzaCrust(event.target.value); }} />
                           <label className="form-check-label" htmlFor="thin">
                             <span>Thin Italian</span> (+$1.50)
                           </label>
                         </div>
                         <div className="form-check topping-items">
-                          <input className="form-check-input" type="radio" name="crust" id="exampleRadios3" value="deep" onChange={ this.handleChange }/>
+                          <input className="form-check-input" type="radio" name="crust" id="exampleRadios3" value="deep" onChange={() => { this.props.pizzaCrust(event.target.value); }}/>
                           <label className="form-check-label" htmlFor="deep">
                             <span>Deep Pan</span> (+$2.50)
                           </label>
