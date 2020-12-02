ad9b9472d88f8ea28007c033403937ba3d7ee628
Mil Amirian <milamirian22@gmail.com>
2020-12-02 01:34:57 +0000

    revised UI

--- client/components/totalFooter.jsx
+++ client/components/totalFooter.jsx
@@ -7,41 +7,54 @@ export default class FooterTotal extends React.Component {
 
     };
     this.showPrice = this.showPrice.bind(this);
+    this.showCals = this.showCals.bind(this);
+
   }
 
   showPrice() {
     const price = this.props.calculatePizzaPrice();
     if (price > 0) {
       return (
-        <span className="top-price">
+        <span className="bottom-price">
             ${(price / 100).toFixed(2)}
         </span>
       );
     } else {
       return (
-        <span className="top-price">
+        <span className="bottom-price">
             $0.00
         </span>
       );
     }
   }
 
+  showCals() {
+    return (
+      <span className='ml-2'>0 total calories</span>
+
+    );
+  }
+
   render() {
     return (
-      <div className="footer-container mt-4">
+      <footer className="footer-container">
         <h1 className='footer-title ml-4'>Your Total</h1>
-        <div className="subtotal-container">
-          <span className="icon">
-            <i className="fas fa-fire-alt"></i>
-          </span>
+        <div className='footer-info-container'>
+          <div className="bottom-subtotal-container">
+            <span className="bottom-icon">
+              <i className="fas fa-fire-alt"></i>
+            </span>
+            <this.showCals/>
+          </div>
+          <div className="bottom-subtotal-container">
+            <span className="bottom-icon">
+              <i className="fas fa-shopping-cart"></i>
+            </span>
+            <this.showPrice/>
+          </div>
         </div>
-        <div className="subtotal-container">
-          <span className="icon">
-            <i className="fas fa-shopping-cart"></i>
-          </span>
-          <this.showPrice/>
-        </div>
-      </div>
+
+      </footer>
     );
   }
 }
