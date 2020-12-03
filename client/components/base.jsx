ff73b33d098210c1740398ff38e3af21ace37da1
Mil Amirian <milamirian22@gmail.com>
2020-12-03 23:46:34 +0000

    revised styling on tiles

--- client/components/base.jsx
+++ client/components/base.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import Tile from './icon-tile';
+// import Tile from './icon-tile';
 
 export default class Base extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      currentPizza: ''
     };
     this.pizza = this.pizza.bind(this);
     this.price = this.price.bind(this);
+    this.renderTile = this.renderTile.bind(this);
+    this.updatedPizzaSelected = this.updatedPizzaSelected.bind(this);
   }
 
   pizza() {
@@ -20,11 +22,78 @@ export default class Base extends React.Component {
   price() {
     const pricing = this.props.basePrice();
     return pricing;
+  }
+
+  updatedPizzaSelected(val) {
+    this.setState({
+      currentPizza: val
+    });
+  }
+
+  renderTile() {
+    const price = this.price();
+
+    const small =
+      <div className={this.state.currentPizza === 'small' ? 'tile-container m-2 tile-select' : 'tile-container m-2'} name="size" id="small" value="small" onClick={() => { this.props.pizzaBase('small'); this.updatedPizzaSelected('small'); }}>
+        <div className='image-bkg'>
+          <img className='tile-image' src="./images/pizza-small.png" alt="small" />
+        </div>
+        <div className='title-container'>
+          <h4 className='title-details' value="small">SMALL</h4>
+          <p className='slices-blurb' value="small">{price.small.slices} <i className="fas fa-pizza-slice fa-lg mr-2"></i></p>
+          <p className='calories-blurb' value="small">{price.small.calPerSlice} Calories per slice</p>
+        </div>
+      </div>;
+
+    const medium =
+      <div className='tile-container m-2' name="size" id="medium" value="medium" onClick={() => { this.props.pizzaBase('medium'); }}>
+        <div className='image-bkg'>
+          <img className='tile-image' src="./images/pizza-medium.png" alt="placeholder" />
+        </div>
+        <div className='title-container'>
+          <h4 className='title-details'>MEDIUM</h4>
+          <p className='slices-blurb'>{price.medium.slices} <i className="fas fa-pizza-slice fa-lg mr-2"></i></p>
+          <p className='calories-blurb'>{price.medium.calPerSlice} Calories per slice</p>
+        </div>
+      </div>;
+
+    const large =
+      <div className='tile-container m-2' name="size" id="large" value="large" onClick={() => { this.props.pizzaBase('large'); }}>
+        <div className='image-bkg'>
+          <img className='tile-image' src="./images/pizza-large.png" alt="placeholder" />
+        </div>
+        <div className='title-container'>
+          <h4 className='title-details'>LARGE</h4>
+          <p className='slices-blurb'>{price.large.slices} <i className="fas fa-pizza-slice fa-lg mr-2"></i></p>
+          <p className='calories-blurb'>{price.large.calPerSlice} Calories per slice</p>
+        </div>
+      </div>;
+
+    const extraLarge =
+      <div className='tile-container m-2 tile-select' name="size" id="extra-large" value="extra-large" onClick={() => { this.props.pizzaBase('extra-large'); }}>
+        <div className='image-bkg'>
+          <img className='tile-image' src="./images/pizza-extra-large.png" alt="placeholder" />
+        </div>
+        <div className='title-container'>
+          <h4 className='title-details'>EXTRA LARGE</h4>
+          <p className='slices-blurb'>{price['extra-large'].slices} <i className="fas fa-pizza-slice fa-lg mr-2"></i></p>
+          <p className='calories-blurb'>{price['extra-large'].calPerSlice} Calories per slice</p>
+        </div>
+      </div>;
+    return (
+      <>
+        {small}
+        {medium}
+        {large}
+        {extraLarge}
+      </>
+    );
 
   }
 
   render() {
     const price = this.price();
+
     return (
       <div className="outer">
         <div className="app">
@@ -55,15 +124,15 @@ export default class Base extends React.Component {
                     <div className="sizing m-2">
                       <h2 className="form-headers pl-4">1. Choose Your Size</h2>
                       <div className="choices ml-4">
-                        <Tile/>
-                        <div className="form-check topping-items">
+                        <this.renderTile/>
+                        {/* <div className="form-check topping-items">
                           <input className="form-check-input" type="radio" name="size" id="small" value="small" onChange={() => { this.props.pizzaBase(event.target.value); }} />
                           <label className="form-check-label" htmlFor="small">
                             <span>Small</span> (6 slices)
                           </label>
                           <p className="price-black">from ${ (price.small.price / 100).toFixed(2) }</p>
-                        </div>
-                        <div className="form-check topping-items">
+                        </div> */}
+                        {/* <div className="form-check topping-items">
                           <input className="form-check-input" type="radio" name="size" id="medium" value="medium" onChange={() => { this.props.pizzaBase(event.target.value); }} />
                           <label className="form-check-label" htmlFor="medium">
                             <span>Medium</span> (8 slices)
@@ -83,7 +152,7 @@ export default class Base extends React.Component {
                             <span>Extra-Large</span> (10 slices)
                           </label>
                           <p className="price-black">from ${ (price['extra-large'].price / 100).toFixed(2) }</p>
-                        </div>
+                        </div> */}
                       </div>
                     </div>
                     <div className="crust m-2">
--- client/components/icon-tile.jsx
+++ client/components/icon-tile.jsx
@@ -4,9 +4,11 @@ export default class Tile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.deselected = this.deselected.bind(this);
+    this.selected = this.selected.bind(this);
   }
 
-  render() {
+  deselected() {
     return (
       <div className='tile-container'>
         <img className='tile-image' src="./images/beef-select.png" alt="placeholder" />
@@ -16,6 +18,25 @@ export default class Tile extends React.Component {
           <p className='calories-blurb'>info goes here</p>
         </div>
       </div>
+    );
+  }
+
+  selected() {
+    return (
+      <div className='tile-container tile-select'>
+        <img className='tile-image' src="./images/beef-select.png" alt="placeholder" />
+        <div className='title-container'>
+          <h4 className='title-details'>size</h4>
+          <p className='slices-blurb'>details goes here</p>
+          <p className='calories-blurb'>info goes here</p>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <this.selected/>
     );
   }
 }
